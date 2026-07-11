627f4866a80c2712f3590c3d66ce57348af7c4a9
mayleeportales <maylee.portales@gmail.com>
2026-07-11 18:47:08 -0300

    w01 programming exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please enter your grade percent: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";            
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately you didn't pass the course... keep practicing and register to the next block!");
+        }
     }
 }
