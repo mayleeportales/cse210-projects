90f3a3594a5556a1d1a3a86a2a18f8b7b149b9ee
mayleeportales <maylee.portales@gmail.com>
2026-08-04 16:52:11 -0300

    W05 Learning Activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignments writingAssignments = new WritingAssignments("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine("====================================================");
+
+        Console.WriteLine(writingAssignments.GetSummary());
+        Console.WriteLine(writingAssignments.GetWritingInformation());
+
+        
     }
 }
---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,14 @@
+public class WritingAssignments : Assignment
+{
+    private string _title;
+
+    public WritingAssignments(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {_studentName}";
+    }
+}
