cc7f2f5052b239b9e9ba3e893c13477095cd2089
mayleeportales <maylee.portales@gmail.com>
2026-08-07 00:01:23 -0300

    W06 learning activity shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -3,7 +3,23 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+    {          
+
+        Square square = new Square("red", 4);
+
+        Rectangle rectangle = new Rectangle("blue", 5, 8);
+
+        Circle circle = new Circle("yellow", 10);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea().ToString("F2"));
+        }
     }
 }
